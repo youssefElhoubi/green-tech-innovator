c23d1d532cc3de0153e280ad6f7faba1565329ad
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-01 20:25:47 +0100

    started of new green team innovators project

---
+++ src/main/java/com/greentechinnovatorsv2/GreenTechInnovatorsV2Application.java
@@ -0,0 +1,13 @@
+package com.greentechinnovatorsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTechInnovatorsV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenTechInnovatorsV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/greentechinnovatorsv2/GreenTechInnovatorsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greentechinnovatorsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenTechInnovatorsV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
